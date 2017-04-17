fcc56e2681dbfb0a88c75783916a058b7f3afa3d
Mishmore <michellemore28@gmail.com>
2017-04-17 17:03:34 -0500

    origen e historia bts ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 var origenArt = document.getElementById("origen-article");
 var origen = document.getElementById("origen-btn").addEventListener("click", function (e) {
-  
+  origenArt.classList.toggle("hidden");
 })
 
 var historiaArt = document.getElementById("historia-article");
 var historia = document.getElementById("historia-btn").addEventListener("click", function(e) {
-  historiaArt.style.display = "none";
+  historiaArt.classList.toggle("hidden");
 })
