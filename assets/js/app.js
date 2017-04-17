b558bdb3bbf293149f18b1b344f3577ea7614d3a
Mishmore <michellemore28@gmail.com>
2017-04-17 16:42:50 -0500

    adding events to btns

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+var origen = document.getElementById("origen-btn").addEventListener("click", function (e) {
+  alert("sdad");
+})
+
+var historia = document.getElementById("historia-btn").addEventListener("click", function(e) {
+  alert("dsfdsf");
+})
