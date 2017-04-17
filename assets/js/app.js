3a5a01b20f6bc755f8c7112b0d1efed54e69b4ad
Mishmore <michellemore28@gmail.com>
2017-04-17 17:12:36 -0500

    close photos ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,14 @@ var historiaArt = document.getElementById("historia-article");
 var historia = document.getElementById("historia-btn").addEventListener("click", function(e) {
   historiaArt.classList.toggle("hidden");
 })
+
+var closeBtns = document.getElementsByClassName("close");
+for (var i=0; i<closeBtns.length; i++) {
+  closeBtns[i].addEventListener("click", function(e) {
+    var parent = e.target.parentNode;
+    parent.classList.add("none");
+    //if (e.target.parentNode){
+    //  e.target.parentNode.remove();
+    //}
+  })
+}
